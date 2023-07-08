306b351596c8158eda07ca563b1f997f0d2113c2
smit-raichura <smit.raichura01@gmail.com>
2023-07-07 21:55:37 -0400

    Coorected Directory Structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 
 // import './App.css'
-import LogIn from './components/Pages/UserLogin/LogIn'
+// import LogIn from './components/Pages/UserLogin/LogIn'
 import SignUp from './components/Pages/UserLogin/Signup'
+import LandingPage from './components/Pages/LandingPage/LandingPage'
+import LogIn from './components/Pages/UserLogin/LogIn'
 
 function App() {
 
 
   return (
     <>
-      <SignUp />
+      <LandingPage />
+      {/* <SignUp />
+      <LogIn /> */}
     </>
   )
 }
--- src/components/Pages/LandingPage/LandingPage.jsx
+++ src/components/Pages/LandingPage/LandingPage.jsx
@@ -1,5 +1,5 @@
-import logo from './assets/logo.png';
-import './App.css';
+import logo from "../../../assets/logo.png"
+import './LandingPage.css';
 
 const LandingPage = () => {
   return (
@@ -7,9 +7,7 @@ const LandingPage = () => {
     <div className="bg-gray-100">
       <nav className="bg-blue-500 py-4 px-5">
         <div className="container mx-auto flex-row space-x-1 content-end">
-          <img className="w-20 h-20" src={logo}/>
-          
-          
+          <img className="w-20 h-20" src={logo}/> 
           <button className="bg-white hover:bg-blue-700 text-blue-500 font-bold py-1 px-4 rounded-full float-right">
            Log in
           </button>
@@ -17,10 +15,7 @@ const LandingPage = () => {
            Sign up
           </button>
         </div>
-        
       </nav>
-
-
       <div className="grid place-items-center">
       <header className="py-16">
         <div className="md:container mx-auto max-w-100">
@@ -36,10 +31,6 @@ const LandingPage = () => {
         </div>
       </header>
       </div>
-      
-      
-      
-      
       <footer className="bg-blue-500 py-4 fixed inset-x-0 bottom-0 p-4">
         <div className="container mx-auto">
           <p className="text-white text-center">© {new Date().getFullYear()} WordWise. All rights reserved.</p>
